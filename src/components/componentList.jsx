ec8aeb7001b31d0a7709c681deda246b353b0cf7
Ming-ux <lvmia0605@gmail.com>
2022-08-17 21:00:15 +0800

    组件布局优化

--- src/components/componentList.jsx
+++ src/components/componentList.jsx
@@ -1,5 +1,9 @@
 import React from "react"
-import componentList from '../schema/components'
+// import componentList from '../schema/components'
+import componentlayoutList from '../schema/components-layout'
+import componenttexttList from '../schema/components-text'
+import componentmultimediaList from '../schema/components-multimedia'
+
 import { DragComponent, Component } from "./render"
 
 
@@ -7,8 +11,29 @@ import { DragComponent, Component } from "./render"
 export function ComponentList() {
     return (
         <section className='editor-left'>
+            
+            
+            <section className='editor-left-layout'>
+                <details open>
+                    <summary>布局</summary>
+                {
+                    componentlayoutList.map((component, index) =>
+                        
+                        <div className='editor-left-item' key={index} style={{ margin: '20px'}}>
+                            <DragComponent key={index}{...component}></DragComponent>
+                            
+                        </div>
+                    )
+                }
+                </details>
+            </section>
+            
+
+            <section className='editor-left-text'>
+                <details open>
+                        <summary>文本</summary>
             {
-                componentList.map((component, index) =>
+                componenttexttList.map((component, index) =>
                     
                     <div className='editor-left-item' key={index} style={{ margin: '20px'}}>
                         <DragComponent key={index}{...component}></DragComponent>
@@ -16,6 +41,32 @@ export function ComponentList() {
                     </div>
                 )
             }
+                </details>
+            </section>
+            <section className='editor-left-multimedia'>
+                <details open>
+                            <summary>多媒体</summary>
+                {
+                    componentmultimediaList.map((component, index) =>
+                        
+                        <div className='editor-left-item' key={index} style={{ margin: '20px'}}>
+                            <DragComponent key={index}{...component}></DragComponent>
+                            
+                        </div>
+                    )
+                }
+                </details>
+            </section>
+
+            {/* {
+                componentList.map((component, index) =>
+                    
+                    <div className='editor-left-item' key={index} style={{ margin: '20px'}}>
+                        <DragComponent key={index}{...component}></DragComponent>
+                        
+                    </div>
+                )
+            } */}
         </section>
     )
 }
---
+++ src/schema/components-text.js
@@ -0,0 +1,109 @@
+export default [
+    {
+        type: 'p',
+        props: {
+            name: '<p>',
+            style: {
+                backgroundColor: 'white'
+            },
+        },
+        children: [
+            '文本',
+        ]
+    },
+    {
+        type: 'span',
+        props: {
+            name: '<span>',
+            style: {
+                backgroundColor: 'white'
+            },
+        },
+        children: [
+            '文本',
+        ]
+    },
+    {
+        type: 'button',
+        props: {
+            name: '<button>',
+            type: 'button',
+            style: {
+                cursor: 'point',
+                color: 'white',
+                backgroundColor: 'white',
+                width: '100px',
+                height: '30px',
+                borderWidth: '1px',
+                backgroundColor: '#3b91fa',
+                borderColor: 'blue',
+                borderRadius: '5px',
+                fontWeight: 'bold'
+
+
+            },
+        },
+        children: [
+            '预览按钮',
+        ]
+    },
+    {
+        type: 'input',
+        props: {
+            name: '<input>',
+            value: '请输入文本',
+            style: {
+            }
+        },
+        children: [
+
+        ]
+    },
+    {
+        type: 'textarea',
+
+        props: {
+            name: '<textarea>',
+            value: 'nihao',
+            style: {
+
+            }
+        },
+        children: [
+
+        ]
+    },
+   
+    {
+        type: 'h1',
+
+        props: {
+            name: '<h1>',
+            style: {
+
+            }
+        },
+        children: [
+            'H1'
+
+        ]
+    },
+
+    {
+        type: 'a',
+        props: {
+            name: '<a>',
+            label: '链接',
+            href: 'https://www.bilibili.com/',
+            style: {
+                cursor: 'point',
+                // textDecoration: 'none',
+            }
+        },
+        children: [
+            '链接',
+
+        ]
+    },
+   
+]
